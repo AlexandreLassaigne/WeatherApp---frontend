cb41e9ab721b4816caa27101339e2c4d5fef3dac
Alexandre <lassaignealexandre@gmail.com>
2025-01-29 17:16:20 +0100

    Finalisation composant Card et modification composant Home

--- components/Card.js
+++ components/Card.js
@@ -4,17 +4,47 @@ import { faHeart, faCircleXmark } from "@fortawesome/free-solid-svg-icons";
 
 export default function Card(props) {
 
+
+  let title = {};
+  if (props.main === "Rain") {
+    title = { color: "#ffffff" };
+  }
+
+  const handleLikeCity = () => {
+    props.updateLikedCity(props.name)
+  }
+
+  let heartStyle = { 'cursor' : 'pointer'}
+  if (props.isLiked){
+    heartStyle = {'cursor' : 'pointer' , 'color': '#e74c3c'}
+  }
+
+  let tempMax = props.tempMax.toFixed(0);
+  let tempMin = props.tempMin.toFixed(0);
+  let cityName = props.name[0].toUpperCase()+ props.name.slice(1)
+
   return (
-    <div className={styles.card} style={{backgroundImage: `url(/${props.main}.gif)`}}>
+    <div
+      className={styles.card}
+      style={{ backgroundImage: `url(/${props.main}.gif)`, height :400}}
+    >
       <div className={styles.header}>
-        <FontAwesomeIcon icon={faHeart} className={styles.heart}/>
-        <p>{props.name}</p>
-        <FontAwesomeIcon icon={faCircleXmark} className={styles.xmark}/>
+        <FontAwesomeIcon icon={faHeart} className={styles.heart}  onClick={handleLikeCity} style={heartStyle}/>
+        <p style={title} className={styles.city}>
+          {cityName}
+        </p>
+        <FontAwesomeIcon icon={faCircleXmark} className={styles.xmark} />
       </div>
-      <span>{props.description}</span>
-      <div className={styles.temp}>
-        <span>TempMax : {props.tempMax} °C</span>
-        <span>TempMin : {props.tempMin} °C</span>
+      <span style={title} className={styles.description}>
+        {props.description}
+      </span>
+      <div className={styles.tempContainer}>
+        <span style={title} className={styles.temp}>
+          TempMin : {tempMin} °C
+        </span>
+        <span style={title} className={styles.temp}>
+          TempMax : {tempMax} °C
+        </span>
       </div>
     </div>
   );
--- components/Home.js
+++ components/Home.js
@@ -5,24 +5,43 @@ import Card from "./Card";
 function Home() {
   const [name, setName] = useState("");
   const [card, setCard] = useState([]);
+  const [likedCity, setLikedCity] = useState([]);
+  const [newCard, setNewCard] = useState([]);
 
-/*   const handleSearch = () => {
-    fetch(`http://localhost:3000/cities/new`)
+  const handleSearch = () => {
+    fetch(`http://localhost:3000/cities/new`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ name: name }),
+    })
       .then((response) => response.json())
       .then((data) => {
-        setCard(data.citie);
+        setNewCard([data.city])
+        setName("");
       });
-  }; */
+  };
 
-  useEffect(() => {
+
+
+  // Liked city (inverse data flow)
+  const updateLikedCity = (cityName) => {
+    if (likedCity.find((city) => city === cityName)) {
+      setLikedCity(likedCity.filter((city) => city !== cityName));
+    } else {
+      setLikedCity([...likedCity, cityName]);
+    }
+  };
+
+/*   useEffect(() => {
     fetch(`http://localhost:3000/cities/all`)
-    .then(response => response.json())
-    .then(data => {   
-      setCard(data.weather)
-    })
+      .then((response) => response.json())
+      .then((data) => {
+        setCard(data.weather);
+      });
+  }, []); */
 
-  }, [])
-  const city = card.map((data, i) => {
+/*   const city = card.map((data, i) => {
+    const isLiked = likedCity.some((city) => city === data.name);
     return (
       <Card
         key={i}
@@ -31,9 +50,28 @@ function Home() {
         description={data.description}
         tempMin={data.tempMin}
         tempMax={data.tempMax}
+        updateLikedCity={updateLikedCity}
+        isLiked={isLiked}
+      />
+    );
+  }); */
+
+  const New = newCard.map((data, i) => {
+    const isLiked = likedCity.some((city) => city === data.name);
+    return (
+      <Card
+        key={i}
+        name={data.name}
+        main={data.main}
+        description={data.description}
+        tempMin={data.tempMin}
+        tempMax={data.tempMax}
+        updateLikedCity={updateLikedCity}
+        isLiked={isLiked}
       />
     );
   });
+
 
   return (
     <div>
@@ -42,17 +80,21 @@ function Home() {
         <div className={styles.searchContainer}>
           <input
             type="text"
-            placeholder="Name"
+            placeholder="City"
             onChange={(e) => setName(e.target.value)}
             value={name}
             className={styles.input}
           />
-          <button className={styles.search}/*  onClick={handleSearch} */>
-            <img src="/glass.png" />
-          </button>
+          <img
+            className={styles.search}
+            src="/glass.png"
+            onClick={handleSearch}
+          />
         </div>
+        <img className={styles.search} src="/user.png" />
       </div>
-      <div className={styles.cityContainer}>{city}</div>
+      {/* <div className={styles.cityContainer}>{city}</div> */}
+      <div className={styles.cityContainer}>{New}</div>
     </div>
   );
 }
