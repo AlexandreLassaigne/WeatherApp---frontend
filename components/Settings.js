25a0d7f5d0ec9c61dd28f04a41e336466fc01aec
Alexandre <lassaignealexandre@gmail.com>
2025-01-31 11:58:03 +0100

    composant settings

--- components/Settings.js
+++ components/Settings.js
@@ -6,10 +6,131 @@ import ListItem from "@mui/material/ListItem";
 import { useRouter } from "next/router";
 import { useState } from "react";
 import { logout } from "../reducers/user";
+import { useSelector, useDispatch } from "react-redux";
+import { updateFirstName, updateLastName } from "../reducers/user";
+import { createPortal } from "react-dom";
+import { removeAllHistory } from "../reducers/history";
 
 export default function Settings() {
   const [open, setOpen] = useState(false);
   const router = useRouter();
+  const user = useSelector((state) => state.user.value);
+  const dispatch = useDispatch();
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [password, setPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [modalFirstNameIsVisible, setModalFirstNameIsVisible] = useState(false);
+  const [modalLastNameIsVisible, setModalLastNameIsVisible] = useState(false);
+  const [modalPasswordIsVisible, setModalPasswordIsVisible] = useState(false);
+  const [messageError, setMessageError] = useState("");
+  const [error, setError] = useState(false);
+
+  const closeModal = (modal) => {
+    if (modal === "password") {
+      setModalPasswordIsVisible(false);
+    } else if (modal === "firstName") {
+      setModalFirstNameIsVisible(false);
+    } else if (modal === "lastName") {
+      setModalLastNameIsVisible(false);
+    }
+  };
+
+  const changePassword = () => {
+    if (!firstName || !lastName || !password || !newPassword) {
+      setMessageError("Please complete all fields");
+      setError(true);
+      return;
+    }
+    fetch("http://localhost:3000/users/changePassword", {
+      method: "PUT",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify({
+        firstName,
+        lastName,
+        password,
+        newPassword,
+        token: user.token,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.result === false) {
+          setMessageError(data.error);
+          setError(true);
+        } else {
+          closeModal("password");
+          setFirstName("");
+          setLastName("");
+          setPassword("");
+          setError(false);
+        }
+      });
+  };
+
+  const changeFirstName = () => {
+    if (!firstName || !lastName || !password) {
+      setMessageError("Please complete all fields");
+      setError(true);
+      return;
+    }
+    fetch("http://localhost:3000/users/changeFirstName", {
+      method: "PUT",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify({
+        firstName,
+        lastName,
+        password,
+        token: user.token,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.result === false) {
+          setMessageError(data.error);
+          setError(true);
+        } else {
+          dispatch(updateFirstName(data.newFirstName));
+          setError(false);
+          closeModal("firstName");
+          setFirstName("");
+          setLastName("");
+          setPassword("");
+        }
+      });
+  };
+
+  const changeLastName = () => {
+    if (!firstName || !lastName || !password) {
+      setMessageError("Please complete all fields");
+      setError(true);
+      return;
+    }
+    fetch("http://localhost:3000/users/changeLastName", {
+      method: "PUT",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify({
+        firstName,
+        lastName,
+        password,
+        token: user.token,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.result === false) {
+          setMessageError(data.error);
+          setError(true);
+        } else {
+          dispatch(updateLastName(data.newLastName));
+          setError(false);
+          closeModal("lastName");
+          setFirstName("");
+          setLastName("");
+          setPassword("");
+        }
+      });
+  };
 
   const handleOpen = (newOpen) => {
     setOpen(newOpen);
@@ -74,6 +195,152 @@ export default function Settings() {
           {drawerList}
         </Drawer>
       </div>
+      <p>My profile</p>
+      <p onClick={() => setModalFirstNameIsVisible(true)}>{user.firstName}</p>
+      {modalFirstNameIsVisible &&
+        createPortal(
+          <div className={styles.containerForm}>
+            <div className={styles.changeContainer}>
+              <div className={styles.close}>
+                <span
+                  onClick={() => closeModal("firstName")}
+                  className={styles.closeButton}
+                >
+                  X
+                </span>
+              </div>
+              <input
+                type="text"
+                placeholder="New FirstName"
+                onChange={(e) => setFirstName(e.target.value)}
+                value={firstName}
+                className={styles.input}
+              />
+              <input
+                type="text"
+                placeholder="LastName"
+                onChange={(e) => setLastName(e.target.value)}
+                value={lastName}
+                className={styles.input}
+              />
+              <input
+                type="password"
+                placeholder="Password"
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+                className={styles.input}
+              />
+              {error && <p className={styles.error}>{messageError}</p>}
+              <button
+                type="submit"
+                onClick={changeFirstName}
+                className={styles.button}
+              >
+                Confirm
+              </button>
+            </div>
+          </div>,
+          document.body
+        )}
+      <p onClick={() => setModalLastNameIsVisible(true)}>{user.lastName}</p>
+      {modalLastNameIsVisible &&
+        createPortal(
+          <div className={styles.containerForm}>
+            <div className={styles.changeContainer}>
+              <div className={styles.close}>
+                <span
+                  onClick={() => closeModal("lastName")}
+                  className={styles.closeButton}
+                >
+                  X
+                </span>
+              </div>
+              <input
+                type="text"
+                placeholder="FirstName"
+                onChange={(e) => setFirstName(e.target.value)}
+                value={firstName}
+                className={styles.input}
+              />
+              <input
+                type="text"
+                placeholder="New LastName"
+                onChange={(e) => setLastName(e.target.value)}
+                value={lastName}
+                className={styles.input}
+              />
+              <input
+                type="password"
+                placeholder="Password"
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+                className={styles.input}
+              />
+              {error && <p className={styles.error}>{messageError}</p>}
+              <button
+                type="submit"
+                onClick={changeLastName}
+                className={styles.button}
+              >
+                Confirm
+              </button>
+            </div>
+          </div>,
+          document.body
+        )}
+      <p onClick={() => setModalPasswordIsVisible(true)}>Change password</p>
+      {modalPasswordIsVisible &&
+        createPortal(
+          <div className={styles.containerForm}>
+            <div className={styles.changeContainer}>
+              <div className={styles.close}>
+                <span
+                  onClick={() => closeModal("password")}
+                  className={styles.closeButton}
+                >
+                  X
+                </span>
+              </div>
+              <input
+                type="text"
+                placeholder="FirstName"
+                onChange={(e) => setFirstName(e.target.value)}
+                value={firstName}
+                className={styles.input}
+              />
+              <input
+                type="text"
+                placeholder="New LastName"
+                onChange={(e) => setLastName(e.target.value)}
+                value={lastName}
+                className={styles.input}
+              />
+              <input
+                type="password"
+                placeholder="Password"
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+                className={styles.input}
+              />
+              <input
+                type="password"
+                placeholder="New password"
+                onChange={(e) => setNewPassword(e.target.value)}
+                value={newPassword}
+                className={styles.input}
+              />
+              {error && <p className={styles.error}>{messageError}</p>}
+              <button
+                type="submit"
+                onClick={changePassword}
+                className={styles.button}
+              >
+                Confirm
+              </button>
+            </div>
+          </div>,
+          document.body
+        )}
     </div>
   );
 }
