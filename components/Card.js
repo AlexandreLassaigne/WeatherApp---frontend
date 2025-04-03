16ef80f005c7cbc9f87a9116519b2a458ac80ae3
Alexandre <lassaignealexandre@gmail.com>
2025-04-03 11:14:41 +0200

    rajout direction du vent

--- components/Card.js
+++ components/Card.js
@@ -30,6 +30,27 @@ export default function Card(props) {
   let tempMin = props.tempMin.toFixed(0);
   let city = props.name[0].toUpperCase() + props.name.slice(1);
   let Wind = props.wind.toFixed(0) * 3.6;
+  let direction
+
+  if(22.6 < props.deg && props.deg< 67.5){
+    direction = 'NE';
+  } else if (67.6 < props.deg && props.deg < 112.5){
+    direction = 'E';
+  } else if (112.6 < props.deg && props.deg < 157.5){
+    direction = 'SE';
+  } else if (157.6 < props.deg && props.deg < 202.5){
+    direction = 'S';
+  } else if (202.6 < props.deg && props.deg < 247.5){
+    direction = 'SO';
+  } else if (247.6 < props.deg && props.deg < 292.5){
+    direction = 'O';
+  } else if (292.6 < props.deg && props.deg < 337.5){
+    direction = 'NO';
+  } else {
+    direction = 'N';
+  }
+
+  console.log(props.deg)
 
   const handleRemoveCity = () => {
     props.handleRemove(props.name);
@@ -73,7 +94,7 @@ export default function Card(props) {
           TempMax : {tempMax} °C
         </span>
         <span style={title} className={styles.temp}>
-          Wind : {Wind} km/h {props.deg}
+          Wind : {Wind} km/h {direction}
         </span>
       </div>
     </div>
