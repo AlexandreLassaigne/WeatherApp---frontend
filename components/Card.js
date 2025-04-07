2c680566d938995537b8affd73988aa510da33c2
Alexandre <lassaignealexandre@gmail.com>
2025-04-07 11:36:37 +0200

    gif

--- components/Card.js
+++ components/Card.js
@@ -29,38 +29,37 @@ export default function Card(props) {
   let tempMin = props.tempMin.toFixed(0);
   let city = props.name[0].toUpperCase() + props.name.slice(1);
   let Wind = props.wind.toFixed(0) * 3.6;
-  let direction
+  let direction;
 
-  if(22.6 < props.deg && props.deg< 67.5){
-    direction = 'NE';
-  } else if (67.6 < props.deg && props.deg < 112.5){
-    direction = 'E';
-  } else if (112.6 < props.deg && props.deg < 157.5){
-    direction = 'SE';
-  } else if (157.6 < props.deg && props.deg < 202.5){
-    direction = 'S';
-  } else if (202.6 < props.deg && props.deg < 247.5){
-    direction = 'SO';
-  } else if (247.6 < props.deg && props.deg < 292.5){
-    direction = 'O';
-  } else if (292.6 < props.deg && props.deg < 337.5){
-    direction = 'NO';
+  if (22.6 < props.deg && props.deg < 67.5) {
+    direction = "NE";
+  } else if (67.6 < props.deg && props.deg < 112.5) {
+    direction = "E";
+  } else if (112.6 < props.deg && props.deg < 157.5) {
+    direction = "SE";
+  } else if (157.6 < props.deg && props.deg < 202.5) {
+    direction = "S";
+  } else if (202.6 < props.deg && props.deg < 247.5) {
+    direction = "SO";
+  } else if (247.6 < props.deg && props.deg < 292.5) {
+    direction = "O";
+  } else if (292.6 < props.deg && props.deg < 337.5) {
+    direction = "NO";
   } else {
-    direction = 'N';
+    direction = "N";
   }
 
   const handleRemoveCity = () => {
     props.handleRemove(props.name);
   };
-  
+
+  const backgroundImage = {
+    backgroundImage: `url('/${props.main}.gif')`,
+    height: 400,
+  };
+
   return (
-    <div
-      className={styles.card}
-      style={{
-        backgroundImage: `url('/${props.main}.gif')`,
-        height: 400,
-      }}
-    >
+    <div className={styles.card} style={backgroundImage}>
       <div className={styles.header}>
         <FontAwesomeIcon
           icon={faHeart}
@@ -70,9 +69,7 @@ export default function Card(props) {
           aria-label="loving the city"
           role="button"
         />
-        <p className={styles.city}>
-          {city}
-        </p>
+        <p className={styles.city}>{city}</p>
         <FontAwesomeIcon
           icon={faCircleXmark}
           className={styles.xmark}
